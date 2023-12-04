e5b6d09dc901c26d920911bc9b430c48c46c33a2
josephson_m <josephson4@yahoo.com>
2023-12-03 21:48:22 -0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".show-cat");
+//console.log(button);
+var cat = document.querySelector(".cat");
+//console.log(cat);
+button.addEventListener("click", function() {
+    //button.innerText = "It's working";
+    cat.classList.add("show");
+});
