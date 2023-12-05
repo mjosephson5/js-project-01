c9082ab74d4c5e3bcf1f7736a734a8909531261e
josephson_m <josephson4@yahoo.com>
2023-12-04 21:57:11 -0800

    Changed button text and add disappearing property

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ var button = document.querySelector(".show-cat");
 //console.log(button);
 var cat = document.querySelector(".cat");
 //console.log(cat);
-button.addEventListener("click", function() {
-    //button.innerText = "It's working";
-    cat.classList.add("show");
-});
+button.addEventListener("click", function () {
+    if (cat.classList.contains("show")) {
+      //console.log("Yes");
+      cat.classList.remove("show");
+      button.innerText = "Wait, come back!";
+      button.classList.add("disappear");
+    } else {
+       //console.log("No");
+       cat.classList.add("show");
+       button.innerText = "Shoo, cat!";
+       button.classList.remove("disappear");
+    }
+  });
+  
